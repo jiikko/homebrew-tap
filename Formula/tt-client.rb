2a1b77ddbcdbb4a5b992d6752d92f6abf7483740
jiikko <n905i.1214@gmail.com>
2026-01-31 01:39:24 +0900

    Update SHA256 for v0.1.0 release

--- Formula/tt-client.rb
+++ Formula/tt-client.rb
@@ -2,7 +2,7 @@ class TtClient < Formula
   desc "ThumbnailThumb API client - Command-line interface for ThumbnailThumb app"
   homepage "https://github.com/jiikko/homebrew-tap"
   url "https://github.com/jiikko/homebrew-tap/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "PLACEHOLDER_SHA256"
+  sha256 "a8b4d8dd655c5273b9dbe1d3db1df3cddceb8dec0a9319d5aa2ff4d1e63b0768"
   version "0.1.0"
   license "MIT"
 
